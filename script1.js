55166cbe1cf2e1e812b3bb2a827b51c780386bfd
0sake <andrea.tatini22@gmail.com>
2023-04-18 17:12:48 +0200

    push to main

---
+++ script1.js
@@ -0,0 +1,21 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("page").style.opacity = "0.4";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("page").style.opacity = "1";
+  }
+
+  function expandSubMenu(mainMenuItem){
+    if(document.getElementById(mainMenuItem).style.display == "none"){
+      document.getElementById(mainMenuItem).style.display = "flex";
+      document.getElementById(mainMenuItem + "-arrow").style.transform = "rotate(90deg)";
+
+    }
+    else{
+      document.getElementById(mainMenuItem).style.display = "none";
+      document.getElementById(mainMenuItem + "-arrow").style.transform = "rotate(0deg)";
+    }
+  }
